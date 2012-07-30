dbd2180cf0ed83c3d40f96db0bee4a4555be5786
Jamie Taylor <seppydude@gmail.com>
2012-07-30 21:20:12 +0100

    book names no read from booklist.xml

--- gen/com/gaprogman/discworldNovelChecklist/R.java
+++ gen/com/gaprogman/discworldNovelChecklist/R.java
@@ -8,30 +8,29 @@
 package com.gaprogman.discworldNovelChecklist;
 
 public final class R {
+    public static final class array {
+        public static final int bookTitles=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int padding_large=0x7f050002;
-        public static final int padding_medium=0x7f050001;
-        public static final int padding_small=0x7f050000;
+        public static final int padding_large=0x7f060002;
+        public static final int padding_medium=0x7f060001;
+        public static final int padding_small=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int checkBoxBookFour=0x7f080004;
-        public static final int checkBoxBookOne=0x7f080001;
-        public static final int checkBoxBookThree=0x7f080003;
-        public static final int checkBoxBookTwo=0x7f080002;
-        public static final int menu_settings=0x7f080005;
-        public static final int welcomeMessage=0x7f080000;
+        public static final int menu_settings=0x7f090001;
+        public static final int welcomeMessage=0x7f090000;
     }
     public static final class layout {
         public static final int activity_check_list_window_window=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_check_list_window_window=0x7f070000;
+        public static final int activity_check_list_window_window=0x7f080000;
     }
     public static final class string {
         public static final int app_name=0x7f040004;
@@ -47,6 +46,6 @@ public final class R {
         public static final int title_activity_check_list_window_window=0x7f040007;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }
